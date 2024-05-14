91267e10bdbee42dacbd905110edcb824ac7c2f3
Daniel998bb <daniel998b@gmail.com>
2024-05-14 18:42:14 +0200

    Actualizar src/App.jsx y agregar src/components

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,16 @@
+import './menu.css';
+
+const Menu = () => {
+    return(
+        <div className= 'contenedor'>  
+            <h1>Ecommerce</h1>
+            <div>
+                <p className='text'>Inicio</p>
+                <p className='text'>Categorias</p>
+                <p className='text'>Contacto</p>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
